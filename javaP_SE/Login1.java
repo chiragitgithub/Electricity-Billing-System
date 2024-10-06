2e5ba4b7a91f41df23e9209cb023a2e786a5f71e
Chirag Gujar <117736144+chiragitgithub@users.noreply.github.com>
2024-10-06 18:37:31 +0530

    Add files via upload

---
+++ javaP_SE/Login1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaP_SE;
+
+/**
+ *
+ * @author X515JAEJ502TS (8503)
+ */
+public class Login1 {
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
